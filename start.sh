38f29dbfb4e071a8b3c9666ce0bf554205ffbc14
小星虎 <105782492+xingxinag@users.noreply.github.com>
2025-10-01 15:06:04 +0800

    运行脚本

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 cat > start.sh << EOF
 #!/bin/sh
-# 如果你有设置主题的需要，可以取消注释下一行，然后按照自己的需求设置。
-export THEME="berry"
+# 如果你有设置主题的需要，可以取消注释下一行（记得把前面的空格也清下），然后按照自己的需求设置。
+# export THEME="berry"
 export TIKTOKEN_CACHE_DIR="$PWD"
-# 把下一行的 PORT 改为自己放行的端口
+# 把下一行的  newapi 与 PORT 改为自己放行的程序文件的名称 与 端口
 exec ./newapi --port 63255 --log-dir ./logs
